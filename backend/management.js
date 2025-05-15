ed2ce9fbb65b90a7c89c8b4eb964f9f6dc195b70
Pierre Njiné Happy <pierrejeremieryan.njinehappy@student.vives.be>
2025-05-15 23:14:51 +0200

    json renewed safely

--- backend/management.js
+++ backend/management.js
@@ -27,7 +27,7 @@ const transporter = nodemailer.createTransport({
 // Google Calendar API Setup
 const SCOPES = ["https://www.googleapis.com/auth/calendar"];
 const calendar = google.calendar("v3");
-const serviceAccountKey = require("./pierino-test-6170eb0ea235.json");
+const serviceAccountKey = JSON.parse(process.env.GOOGLE_CREDENTIALS_JSON);
 const auth = new google.auth.GoogleAuth({
   credentials: serviceAccountKey,
   scopes: SCOPES,
