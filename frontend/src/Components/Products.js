330e93b9a9772ccb29eaeb21e9a1f950b39babf8
Pierre Njiné Happy <pierrejeremieryan.njinehappy@student.vives.be>
2025-07-02 14:06:40 +0200

    link option add to homepage

--- frontend/src/Components/Products.js
+++ frontend/src/Components/Products.js
@@ -1,39 +1,39 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import './Products.css'; // Assuming the styles are in this file
+import "./Products.css"; // Assuming the styles are in this file
 // Import images correctly
-import iceCream1x from './images/ice-cream-header-d.png';
-import iceCream2x from './images/ice-cream-header-d@2x.png';
-import milk1x from './images/milk-header-d.png';
-import milk2x from './images/milk-header-d@2x.png';
-import girlIceCream1x from './images/girl-icecream-d.png';
-import girlIceCream2x from './images/girl-icecream-d@2x.png';
-import arrowRight from './images/arrow-right.png';
+import iceCream1x from "./images/ice-cream-header-d.png";
+import iceCream2x from "./images/ice-cream-header-d@2x.png";
+import milk1x from "./images/milk-header-d.png";
+import milk2x from "./images/milk-header-d@2x.png";
+import girlIceCream1x from "./images/girl-icecream-d.png";
+import girlIceCream2x from "./images/girl-icecream-d@2x.png";
+import arrowRight from "./images/arrow-right.png";
 // Import symbols correctly
-import symbols from './images/symbols/symbol-defs.svg'; 
+import symbols from "./images/symbols/symbol-defs.svg";
 import backgroundImage from "../Components/images/bgIceCream.png"; // Background image
 
 const Products = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
 
-    const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
 
-    useEffect(() => {
-      const handleResize = () => {
-        setIsMobile(window.innerWidth < 768);
-      };
-  
-      window.addEventListener("resize", handleResize);
-      return () => window.removeEventListener("resize", handleResize);
-    }, []);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
   return (
-    
     <section className="products-section-products" id="products">
-      <div className="products-div-products" style={{
-        backgroundImage: `url(${backgroundImage})`,
-        backgroundPosition: "center",
-        backgroundSize: isMobile ? "auto" : "cover", // Dynamic background size
-      }}
-    >
+      <div
+        className="products-div-products"
+        style={{
+          backgroundImage: `url(${backgroundImage})`,
+          backgroundPosition: "center",
+          backgroundSize: isMobile ? "auto" : "cover", // Dynamic background size
+        }}
+      >
         <div className="titles-products">
           <p className="product-title">100% ambachtelijke</p>
           <h2 className="products-title">PRODUCTEN</h2>
@@ -45,67 +45,63 @@ const Products = () => {
               <use href={`${symbols}#icon-dots-dots`}></use>
             </svg>
             <p className="paragraph-strawberry-product">
-            Zin in verfrissend, ambachtelijk ijs? Bestel nu en trakteer jezelf op de lekkerste smaken! 
+              Zin in verfrissend, ambachtelijk ijs? Bestel nu en trakteer jezelf
+              op de lekkerste smaken!
             </p>
             <div className="icons">
               <svg className="arrow-right-product">
-                <use
-                  href="../src/images/symbols/symbol-defs.svg#icon-arrow-small"
-                ></use>
+                <use href="../src/images/symbols/symbol-defs.svg#icon-arrow-small"></use>
               </svg>
             </div>
             <div className="product__overlay">
               <p className="product__text">
-              Of je nu thuis geniet of een feest plant, wij zorgen voor het perfecte ijsmoment. ❄️✨
+                Of je nu thuis geniet of een feest plant, wij zorgen voor het
+                perfecte ijsmoment. ❄️✨
               </p>
             </div>
           </div>
 
-          <Link to="/offer">
-          <div className="rectangle-2-product">
-            <h3 className="coffee-title-product">Reserveren</h3>
-            <svg className="dots-product">
-            <use href={`${symbols}#icon-dots-dots`}></use>
-            </svg>
-            <p className="paragraph-coffee-product">
-            Reserveer jouw ijskar vandaag! 
-
-Maak je evenement extra speciaal met onze ijskar vol heerlijk ambachtelijk ijs! 
-            </p>
-            <div className="icons">
-              <svg className="arrow-right-product">
-                <use
-                  href="../src/images/symbols/symbol-defs.svg#icon-arrow-small"
-                ></use>
+          <Link to="/offer" className="rectangle-2-product">
+            <div className="rectangle-2-product">
+              <h3 className="coffee-title-product">Reserveren</h3>
+              <svg className="dots-product">
+                <use href={`${symbols}#icon-dots-dots`}></use>
               </svg>
-            </div>
-            <div className="product__overlay">
-              <p className="product__text">
-              Of het nu gaat om een feest, bruiloft of bedrijfsbijeenkomst – wij zorgen voor een onvergetelijke ervaring.
+              <p className="paragraph-coffee-product">
+                Reserveer jouw ijskar vandaag! Maak je evenement extra speciaal
+                met onze ijskar vol heerlijk ambachtelijk ijs!
               </p>
+              <div className="icons">
+                <svg className="arrow-right-product">
+                  <use href="../src/images/symbols/symbol-defs.svg#icon-arrow-small"></use>
+                </svg>
+              </div>
+              <div className="product__overlay">
+                <p className="product__text">
+                  Of het nu gaat om een feest, bruiloft of bedrijfsbijeenkomst –
+                  wij zorgen voor een onvergetelijke ervaring.
+                </p>
+              </div>
             </div>
-          </div>
           </Link>
           <div className="rectangle-3-product">
             <h3 className="milkshake-title-product">Contacteren</h3>
             <svg className="dots-product">
-            <use href={`${symbols}#icon-dots-dots`}></use>
+              <use href={`${symbols}#icon-dots-dots`}></use>
             </svg>
             <p className="paragraph-milkshake-product">
-            Heb je vragen, wil je een bestelling plaatsen of een ijskar reserveren? Neem gerust contact met ons op!
+              Heb je vragen, wil je een bestelling plaatsen of een ijskar
+              reserveren? Neem gerust contact met ons op!
             </p>
             <div className="icons">
               <svg className="arrow-right-product">
-                <use
-                  href="../src/images/symbols/symbol-defs.svg#icon-arrow-small"
-                ></use>
+                <use href="../src/images/symbols/symbol-defs.svg#icon-arrow-small"></use>
               </svg>
             </div>
             <div className="product__overlay">
               <p className="product__text">
-              Wij helpen je met plezier verder.
-
- Stuur ons een bericht en we reageren zo snel mogelijk! 
+                Wij helpen je met plezier verder. Stuur ons een bericht en we
+                reageren zo snel mogelijk!
               </p>
             </div>
           </div>
@@ -115,4 +111,4 @@ Maak je evenement extra speciaal met onze ijskar vol heerlijk ambachtelijk ijs!
   );
 };
 
-export default Products
+export default Products;
