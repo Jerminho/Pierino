dd87832219b72a61a7d37cf337c11e10ac55f78c
Pierre Njiné Happy <pierre.wy@outlook.fr>
2026-07-13 01:31:46 +0200

    Add thank you page and route

--- frontend/src/Pages/ThankYouPage.js
+++ frontend/src/Pages/ThankYouPage.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Header from "../Components/Header";
-import Footer from "../Components/Footer";
 import backgroundImage from "../Components/images/bgIceCream.png";
 
 function ThankYouPage() {
